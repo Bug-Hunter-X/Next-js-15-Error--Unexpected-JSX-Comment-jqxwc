fb198138726769366026cf2223d34cec655809b7
BugHunter-X <askzenai@gmail.com>
2025-01-02 11:36:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      {/* This comment causes the error in Next.js 13 */}      
+    </div>
+  );
+}
+```
